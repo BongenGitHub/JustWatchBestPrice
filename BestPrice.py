acc9f76a1d13bd1cf61a435a21ac272b26d5352f
Eugen <eugen.bonet@me.com>
2019-05-11 08:48:53 +0000

    all languages added, extract movie name

--- BestPrice.py
+++ BestPrice.py
@@ -1,14 +1,24 @@
 from justwatch import JustWatch
 
+def getMovieName(url):
+	parts = url.split('/')
+	return parts[len(parts) -1]
+
+
+url = "https://www.justwatch.com/de/Film/Aquaman"
+
 # No English Sound: MX, BR, RU
-# No Func: NL, NO, SE, DK, LT, LV, EE, IN, TH, MY, PH, ID
 nations = ["US", "CA", "DE", "AT", "CH"
 					,"GB", "IE", "IT", "FR", "ES"
-					,"FI", "PT", "PL", "ZA", "AU"
-					,"NZ", "JP", "KR", "SG"]
+					,"NL", "NO", "SE", "DK", "FI"
+					,"LT", "LV", "EE", "PT", "PL"
+					,"ZA", "AU","NZ", "IN", "JP"
+					,"KR", "TH", "MY", "PH" "SG", "ID"]
+					
+					
+					
+movieName = getMovieName(url)
 
-jw = JustWatch(country='ID')
-
-results = jw.search_for_item(query='aquaman')
-
-print("hello")
+for i in nations:
+	jw = JustWatch(country=i)
+	results = jw.search_for_item(query=movieName)
